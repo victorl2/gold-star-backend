952fce1776522b1f4e2cfaf8c807a1fa5a90d9c6
Victor Silva <victorfts@id.uff.br>
2018-08-18 16:50:58 -0300

    criando repository para a classe ImovelResidencial e atualizando implementacao dentro do BaseDAO e Repository

---
+++ bin/README.md
@@ -0,0 +1,3 @@
+## GoldStar Backend
+
+Backend para aplicação de gerenciamento de imóveis no edifício gold star.
---
+++ src/main/java/domain/repository/ImovelResidencialRepository.java
@@ -0,0 +1,7 @@
+package domain.repository;
+
+import domain.entity.negocio.ImovelResidencial;
+
+public interface ImovelResidencialRepository extends Repository<ImovelResidencial> {
+
+}
