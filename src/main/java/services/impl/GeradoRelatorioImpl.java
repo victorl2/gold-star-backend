584e52f47860d438872d607a0001327020be5cbf
ronald <ronaldcampbell@id.uff.br>
2018-08-18 16:15:19 -0300

    Adicionando dependencia IText

--- src/main/java/services/impl/GeradoRelatorioImpl.java
+++ src/main/java/services/impl/GeradoRelatorioImpl.java
@@ -1,11 +1,29 @@
 package services.impl;
 
+import java.io.FileNotFoundException;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
 import domain.entity.negocio.Relatorio;
 import services.GeradorRelatorio;
 
 public class GeradoRelatorioImpl implements GeradorRelatorio{
 
 	public Relatorio gerarRelatorioImoveisResidenciais() {
+		PdfWriter writer;
+		try {
+			writer = new PdfWriter("C:\\Users\\ronal\\Documents\\projeto goldstar\\gold-star-backend");
+			PdfDocument pdf = new PdfDocument(writer);
+			Document document = new Document(pdf);
+			document.add(new Paragraph("Hello World!"));
+			document.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 		throw new UnsupportedOperationException("Funcionalidade não implementada");
 	}
 
