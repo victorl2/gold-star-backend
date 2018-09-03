3cce3b06c7a38f3595a94eb5ced9214b42c8e08b
ronald <ronaldcampbell@id.uff.br>
2018-09-02 21:14:42 -0300

    Correção da busca para solucionar jackson cyclic

--- src/main/java/domain/entity/negocio/Proprietario.java
+++ src/main/java/domain/entity/negocio/Proprietario.java
@@ -7,6 +7,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Proprietario extends Pessoa {
 	private static final long serialVersionUID = -173814518654814808L;
@@ -17,6 +19,7 @@ public class Proprietario extends Pessoa {
 	@Column(name = "ENDERECO")
 	private String endereco;
 	
+	@JsonBackReference
 	@OneToMany(mappedBy = "donoImovel")
 	private List<Imovel> imoveis;
 
