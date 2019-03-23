d4e7707371977d1df7fd267c45984bdeaa64599f
Victor Silva <victorfts@id.uff.br>
2019-03-23 17:55:02 -0300

    otimizando buscas no imovel comercial

--- src/main/java/domain/dao/ImovelComercialDAO.java
+++ src/main/java/domain/dao/ImovelComercialDAO.java
@@ -1,5 +1,7 @@
 package domain.dao;
 
+import java.util.List;
+
 import domain.entity.negocio.ImovelComercial;
 import domain.repository.ImovelComercialRepository;
 
@@ -7,5 +9,35 @@ import domain.repository.ImovelComercialRepository;
  * Implementação das definições de persistencia para a classe <b>ImovelComercial</b>
  */
 public class ImovelComercialDAO extends BaseDAO<ImovelComercial> implements ImovelComercialRepository{
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ImovelComercial> buscarImovelComercialPorNumero(String numeroImovel) {
+		return getEntityManager()
+			.createQuery("SELECT residencia FROM ImovelComercial residencia WHERE residencia.numeroImovel LIKE :numeroResidencia")
+				.setParameter("numeroResidencia", "%" + numeroImovel + "%")
+					.setMaxResults(MAX_RESULTS)
+						.getResultList();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ImovelComercial> buscarImovelComercialPorRGI(String RGI) {
+		return getEntityManager()
+				.createQuery("SELECT residencia FROM ImovelComercial residencia WHERE residencia.rgi LIKE :rgi")
+					.setParameter("rgi", "%" + RGI + "%")
+						.setMaxResults(MAX_RESULTS)
+							.getResultList();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ImovelComercial> buscarImovelComercialPorNomeLocatario(String nomeLocatario) {
+		return getEntityManager()
+				.createQuery("SELECT residencia FROM ImovelComercial residencia WHERE residencia.locador.nome LIKE :locatario")
+					.setParameter("locatario", "%" + nomeLocatario + "%")
+						.setMaxResults(MAX_RESULTS)
+							.getResultList();
+	}
 
 }
--- src/main/java/domain/repository/ImovelResidencialRepository.java
+++ src/main/java/domain/repository/ImovelResidencialRepository.java
@@ -10,21 +10,21 @@ import domain.entity.negocio.ImovelResidencial;
 public interface ImovelResidencialRepository extends Repository<ImovelResidencial> {
 	
 	/**
-	 * Busca todos os imóveis que possuem o número informado
+	 * Busca todos os imóveis resindenciais que possuem o número informado
 	 * @param numeroImovel
 	 * @return {@link List} de {@link ImovelResidencial}
 	 */
 	List<ImovelResidencial> buscarImovelResidencialPorNumero(final String numeroImovel);
 	
 	/**
-	 * Busca todos os imoveis com o rgi informado
+	 * Busca todos os imoveis resindenciais com o rgi informado
 	 * @param RGI para filtragem
 	 * @return {@link List} de {@link ImovelResidencial}
 	 */
 	List<ImovelResidencial> buscarImovelResidencialPorRGI(final String RGI);
 	
 	/**
-	 * Busca todos os imoveis com o nome de locatario informado
+	 * Busca todos os imoveis resindenciais com o nome de locatario informado
 	 * @param nomeLocatario para filtragem
 	 * @return {@link List} de {@link ImovelResidencial}
 	 */
